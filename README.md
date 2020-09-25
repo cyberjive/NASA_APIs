e421b7ddf725330848e5c4f8831083b9ebbb1f0a
cyberjive <jputte49@hotail.com>
2020-09-25 17:28:21 -0500

    Inital updates, apod and landsat skeletons

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-Playing around with NASA APIs...
+## NASA APIS
+
+Playing around with requests and NASA APIs, will probably it into a 
+Flask thing
+
+## Installation
+
+Clone, create your venv, install dependencies
+
+```bash
+git clone https://github.com/cyberjive/NASA_APIs.git
+python3 -m venv env
+source env/bin/activate
+python -m pip install -r requirements.txt
+```
+
+## Usage
+
+Activate your virtual environment and run the script(s)
+
+
+```bash
+source env/bin/activate
+python3 app.py
+```
--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+#! /usr/bin/python
+
 # standard library
 import os
 import json
@@ -14,18 +16,39 @@ def get_apod():
     Call the astronomy pic of the day endpoint and return the JSON
     """
     url = f"https://api.nasa.gov/planetary/apod?api_key={ NASA_KEY }"
-    response = requests.get(url)
-    if response.ok:
-        response_json = json.loads(response.text)
-        json_string = json.dumps(
-            response_json,
-            indent=4,
-            sort_keys=True,
-        )
-        print(json_string)
-        return json_string
-    else:
-        return f"Error on call, response code: { response.status_code }"
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            response_json = json.loads(response.text)
+            json_string = json.dumps(
+                response_json,
+                indent=4,
+                sort_keys=True,
+            )
+            print(json_string)
+            return json_string
+        else:
+            return f"Error on call, response { response.status_code }"
+    except Exception as e:
+        return f"Error: { str(e) }"
+
+
+def get_landsat_imagery(lat=40.647040, lon=-73.937414):
+    """
+    Call the landsat imagery endpoint and return the result
+    """
+    url = f"https://api.nasa.gov/planetary/earth/imagery?lon={ lon }&lat={ lat }&api_key={ NASA_KEY }"
+    try:
+        response = requests.get(url)
+        if response.ok:
+            print(response.content)
+        else:
+            return f"Error on call, response code { response.status_code }"
+    except Exception as e:
+        return f"Error: { str(e) }"
+
 
 if __name__ == "__main__":
-    get_apod()
+    # get_apod()
+    # get_landsat_imagery()
+    pass
